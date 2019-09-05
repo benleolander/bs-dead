1e7114bf0d258d8ad842e499f9397ebc83e1eeba
Ben Lander <ben.l.lander@googlemail.com>
2019-09-05 16:55:03 +0100

    Axios req returns true/false as expected

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 console.log('JS linked')
-
+require('babel-polyfill')
 import axios from 'axios'
 
-let bruce
+// let bruce
+//
+// function isBruceDead() {
+//   axios
+//     .get()
+//     .then(res => {
+//       bruce = res.data.d.results[0]
+//     })
+// }
 
-axios
-  .get('http://dbpedia.org/data/Bruce_Springsteen.jsod')
-  .then(res => {
-    bruce = res.data.d.results[0]
-    console.log('BRUCE DIED ', bruce['http://dbpedia.org/ontology/deathDate'])
-  })
+const isBruceDead = async () => {
+  try {
+    const res = await axios.get('http://dbpedia.org/data/Bruce_Springsteen.jsod')
+
+    const bruce = res.data.d.results[0]
+
+    if (bruce['http://dbpedia.org/ontology/deathDate']) console.log('Dead')
+    else return console.log('Not Dead')
+
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+isBruceDead()
